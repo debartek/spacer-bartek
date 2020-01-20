6d5c2b9cad9d0c68d0c6eb8ec3614aa7f5d90908
Bartłomiej Dembiński <bartlomiej.dembinski@suus.com>
2020-01-20 20:54:46 +0100

    Modify indentation to 2 spaces close router markup

--- src/router.js
+++ src/router.js
@@ -3,20 +3,18 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
 
-
-
 Vue.use(Router);
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home,
-        },
-        {
-            path: '/about',
-            name: 'About',
-            component: About,
-        },
-    ],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+  ],
 });
